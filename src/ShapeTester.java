eeb932298d29c7836b31720db64c602983b7ac05
Simon Sall Andersen <ssalla@ruc.dk>
2021-09-27 14:52:24 +0200

    Adding comments to ShapeTester.java

--- src/ShapeTester.java
+++ src/ShapeTester.java
@@ -1,12 +1,19 @@
 public class ShapeTester {
     public static void main(String[] args)
     {
+        // Testing Circle
         Circle c = new Circle(4.0);
 
+        // Calculate area of Circle
         c.CalcArea();
+
+        // Calculate perimeter of Circle
         c.CalcPerimeter();
+
+        // Print out Shape details
         c.showShape();
 
+        // Print out the center point of Shape
         c.CircleCenter(-1, -2,7,4);
     }
 }
