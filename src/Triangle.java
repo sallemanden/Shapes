c2af05a32f5bcf87dac0dcf852d319f07144d2b2
ctofte <ctofte@ruc.dk>
2021-09-28 17:41:32 +0200

    Added Triangle class (kinda)

---
+++ src/Triangle.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+
+public class Triangle extends Shape{
+
+    double width, length;
+    double cathetusA = length;
+    double cathetusB = width;
+
+    public Triangle(){
+
+        name = "Triangle";
+        width = 0;
+        length = 0;
+
+    }
+
+    public Triangle(double triWidth, double triLength){
+
+        name = "Rectangle";
+        width = triWidth;
+        length = triLength;
+    }
+
+    public void calcArea(){
+
+        area = width * length / 2;
+    }
+
+    public void calcPerimeter(){
+
+    }
+}
