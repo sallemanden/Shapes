f074ab3ec7deed46d8d34602671063bb8376e3ab
ctofte <ctofte@ruc.dk>
2021-09-29 13:33:45 +0200

    Added comments to Triangle and TriangleTester

--- src/Triangle.java
+++ src/Triangle.java
@@ -16,6 +16,8 @@ public class Triangle extends Shape {
     }
 
     public void calcDistance(int x1, int y1, int x2, int y2, int x3, int y3) {
+
+        //calculate distance between the three points giving the sides of the triangle
         sideAB = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
         sideBC = Math.sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
         sideCA = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
@@ -23,6 +25,7 @@ public class Triangle extends Shape {
         System.out.println("Side BC: " + sideBC);
         System.out.println("Side CA: " + sideCA);
 
+        //calculates the average distance between the three points
         double d = (sideAB + sideBC + sideCA) / 3;
         System.out.println(d);
 
--- src/TriangleTester.java
+++ src/TriangleTester.java
@@ -2,10 +2,13 @@ public class TriangleTester {
     public static void main(String[] args){
         Triangle t = new Triangle();
 
+        //calculates the distance between the three points giving the sides of the triangle
         t.calcDistance(50, 0,50,100,100, 0);
 
+        //calculate the area of the triangle
         t.calcArea();
 
+        //calculate the perimeter of the triangle
         t.calcPerimeter();
 
     }
