dd6e2c184d7aa6fbaf4ee13616048092e95bb656
ctofte <ctofte@ruc.dk>
2021-09-29 13:25:18 +0200

    Added TriangleTester.java class

---
+++ src/TriangleTester.java
@@ -0,0 +1,11 @@
+public class TriangleTester {
+    public static void main(String[] args){
+        Triangle t = new Triangle();
+
+        t.calcDistance(50, 0,50,100,100, 0);
+
+        t.calcArea();
+
+        t.calcPerimeter();
+    }
+}
